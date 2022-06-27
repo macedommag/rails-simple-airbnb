af19ebafac028ae4bdf5b7b21fcaaa4b687c1055
Gabriel Magalhaes <macedommag@gmail.com>
2022-06-27 20:24:19 -0300

    create seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_185717) do
+ActiveRecord::Schema.define(version: 2022_06_27_224234) do
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "addres"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "simples", force: :cascade do |t|
     t.string "Airbnb"
